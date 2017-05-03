8e150e611fbdfd7fd4b4af6c4dda12b9ac0e676b
John Williamson <johnwilliamson@Johns-MacBook-Air-5.local>
2017-05-03 10:49:35 +1000

    installed web3

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Web3 from 'web3';
+
+const ETHEREUM_CLIENT = new Web3(new Web3.providers.HttpProvider('http://localhost:8545'))
+const peopleContractAbi = null;
+const peopleAddress = null;
 
 class App extends Component {
   render() {
+    console.log(ETHEREUM_CLIENT);
     return (
       <div className="App">
         <div className="App-header">
@@ -13,6 +19,20 @@ class App extends Component {
         <p className="App-intro">
           Read and write your name to the blockchain!
         </p>
+        <div className="people-table">
+          <table>
+            <tr>
+              <th>Name</th>
+              <th>Age</th>
+              <th>Address</th>
+            </tr>
+            <tr>
+              <td>Goes here</td>
+              <td>Goes here</td>
+              <td>Goes here</td>
+            </tr>
+          </table>
+        </div>
       </div>
     );
   }
